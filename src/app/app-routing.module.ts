ab99e534be456217e8f0b45ff38acee68354b52d
lectrabp <pbgiovanny@yahoo.com>
2021-05-02 18:00:14 -0500

    first commit sort folders project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component:DashboardComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'}, 
+  {path:'**' , component: NotFoundPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumpsComponent } from './shared/breadcrumps/breadcrumps.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    BreadcrumpsComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
